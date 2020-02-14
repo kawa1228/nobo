3a8e3f4b11724260f4c5f9967cbfd4fae6af2599
kawa1228 <sxa.1105bo@gmail.com>
2020-02-15 01:11:02 +0900

    workページの画像を追加

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="work">
-    <h1>This is an work page</h1>
-  </div>
+  <section class="work">
+    <div class="contents">
+      <img src="/img/card_01.png" alt="">
+      <img src="/img/card_02.png" alt="">
+      <img src="/img/card_03.png" alt="">
+      <img src="/img/card_04.png" alt="">
+      <img src="/img/card_05.png" alt="">
+      <img src="/img/card_06.png" alt="">
+      <img src="/img/card_07.png" alt="">
+      <img src="/img/card_08.png" alt="">
+      <img src="/img/card_09.png" alt="">
+    </div>
+  </section>
 </template>
 
 <script>
@@ -10,5 +20,15 @@
 
 <style lang="scss" scoped>
 .work {
+  .contents {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 4.3rem;
+
+    img {
+      width: 100%;
+    }
+  }
 }
 </style>

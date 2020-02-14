b56dbed2984b3d5782fa5aa990d8bfe9b28a5898
kawa1228 <sxa.1105bo@gmail.com>
2020-02-15 01:22:16 +0900

    pageup iconを追加

---
+++ src/components/Pageup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pageup">
+    <PageupIcon class="pageup-icon"/>
+  </div>
+</template>
+
+<script>
+import PageupIcon from '@/assets/svg/page-up.svg'
+
+export default {
+  name: 'Pageup',
+  components: {
+    PageupIcon
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.pageup {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &-icon {
+    width: 8rem;
+    height: 8rem;
+  }
+}
+</style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -11,16 +11,26 @@
       <img src="/img/card_08.png" alt="">
       <img src="/img/card_09.png" alt="">
     </div>
+    <Pageup/>
   </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import Pageup from '@/components/Pageup'
+
+export default {
+  name: 'Work',
+  components: {
+    Pageup
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .work {
   .contents {
+    margin: 20.3rem 0 17rem;
     display: grid;
     grid-template-rows: 1fr 1fr 1fr;
     grid-template-columns: 1fr 1fr 1fr;

dfe36a68d32ded4aa79569389ea71524d88a7058
kawa1228 <sxa.1105bo@gmail.com>
2020-02-15 04:45:14 +0900

    記事中の画像sampleを表示

--- src/components/WorkItems/Lipton1.vue
+++ src/components/WorkItems/Lipton1.vue
@@ -1,13 +1,12 @@
 <template>
-  <section class="work-item">
-    <h1>lipton 1</h1>
-    <article>
+  <article class="work-item flex-center">
+    <div class="contents">
       <p>Do est reprehenderit commodo culpa. Exercitation reprehenderit nisi voluptate eiusmod cupidatat pariatur cupidatat enim elit pariatur consequat in velit consectetur.</p>
       <p>In do tempor officia elit sunt. Irure officia dolor consectetur cupidatat.</p>
-      <br>
+      <img src="/img/work/01/article/sample.png" alt="">
       <p>Mollit laboris nisi nostrud reprehenderit minim ex. Pariatur aliquip in Lorem labore tempor irure voluptate incididunt cupidatat quis incididunt elit esse quis. Consectetur ipsum aliquip proident officia ut quis qui esse tempor consequat eiusmod in ipsum dolor. Non duis ipsum ipsum ullamco enim irure commodo proident incididunt.</p>
-    </article>
-  </section>
+    </div>
+  </article>
 </template>
 
 <script>
@@ -19,5 +18,14 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .work-item {
+  margin: 7.4rem 0;
+
+  .contents {
+    width: 40%;
+
+    img {
+      margin: 7rem 0;
+    }
+  }
 }
 </style>

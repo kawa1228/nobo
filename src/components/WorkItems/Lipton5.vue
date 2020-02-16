648cbac0290cb131cb8e04bf1a7676f61afa88a6
kawa1228 <sxa.1105bo@gmail.com>
2020-02-16 21:50:22 +0900

    コンポーネントを追加

---
+++ src/components/WorkItems/Lipton5.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="work-item">
+    <div>lipton 5 の記事がここに入ります</div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Lipton5',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.work-item {
+}
+</style>
---
+++ src/components/WorkItems/Lipton6.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="work-item">
+    <div>lipton 6 の記事がここに入ります</div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Lipton6',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.work-item {
+}
+</style>
